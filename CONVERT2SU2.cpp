bc40b1584fedb2f1e51e401726ce9352be96c796
mima619 <m-minervino@users.noreply.github.com>
2024-10-29 14:13:51 +0100

    Included program description, copyright and license information.

--- CONVERT2SU2.cpp
+++ CONVERT2SU2.cpp
@@ -1,3 +1,26 @@
+	/* CONVERT2SU2 program, version 1.0
+	The program reads in a PLOT3D file containing the mesh
+	(it can be either a direct output from CONSTRUCT2D or a coarsened mesh file from COARSEN_P2D)
+	and a second real argument (scale_f) that specifies the grid scaling to be applied to the output.
+	The code then provides, as an output, a new grid file in the native format used by the
+	Stanford University Unstructured (SU2) suite (https://su2code.github.io/), which can be used
+	for example to simulate the flow-field around the selected aerofoil using its flow solver module (SU2_CFD).
+	Copyright (C) 2024  Mauro Minervino
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
