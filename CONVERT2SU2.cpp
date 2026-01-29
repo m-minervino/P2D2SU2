a7ecfbe8a205872b4da034da3218ec0931e32fd6
Mauro Minervino <m-minervino@users.noreply.github.com>
2026-01-29 18:39:45 +0100

    Added grid rotation and modified PLOT3D output file

--- CONVERT2SU2.cpp
+++ CONVERT2SU2.cpp
@@ -1,10 +1,13 @@
 	/* CONVERT2SU2 program, version 1.0
 	The program reads in a PLOT3D file containing the mesh
 	(it can be either a direct output from CONSTRUCT2D or a coarsened mesh file from COARSEN_P2D)
-	and a second real argument (scale_f) that specifies the grid scaling to be applied to the output.
+	and two optional arguments:
+	1) scale_factor (default: 1.0), specifying any grid scaling applied to the output.
+	2) rot_angle (default: 0.0), specifying any grid rotation (along -Z axis) applied to the output.
 	The code then provides, as an output, a new grid file in the native format used by the
 	Stanford University Unstructured (SU2) suite (https://su2code.github.io/), which can be used
 	for example to simulate the flow-field around the selected aerofoil using its flow solver module (SU2_CFD).
+	If any of the optional argument are specified differently from defaults, the modified PLOT3D grid is also written.
 	Copyright (C) 2024  Mauro Minervino
 
     This program is free software; you can redistribute it and/or modify
@@ -24,9 +27,10 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <vector>
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 using namespace std;
 int main(int argc, char** argv) {
@@ -34,9 +38,15 @@ int main(int argc, char** argv) {
 int ni=0;
 int nk=0;
 double scale_factor=1;
+double rot_angle=0;
 if (argc > 2) {
        scale_factor = atof(argv[2]);
-       cout << "scale factor:" << scale_factor << "\n";
+       cout << "scaling factor:" << scale_factor << "\n";
+}
+if (argc > 3) {
+       rot_angle = atof(argv[3]);
+       cout << "rotation angle:" << rot_angle << " degrees\n";
+	   rot_angle = rot_angle*M_PI/180.0;
 }
 
 ifstream in_file(argv[1]);
@@ -79,6 +89,44 @@ for (int v = 0; v < 2; v++) {
 	}
 }
 
+if (scale_factor!=1) {
+	for (int v = 0; v < 2; v++) {
+		for (int k = 0; k < nk; k++) {
+			for (int i = 0; i < ni; i++) {
+				values[i][k][v] =  values[i][k][v]*scale_factor;
+			}
+		}
+	}
+}
+
+if (rot_angle!=0) {
+	double tmp=NAN;
+	for (int k = 0; k < nk; k++) {
+        for (int i = 0; i < ni; i++) {
+			tmp=values[i][k][0];
+			values[i][k][0] =  values[i][k][0]*cos(rot_angle)+values[i][k][1]*sin(rot_angle);
+			values[i][k][1] = -tmp*sin(rot_angle)+values[i][k][1]*cos(rot_angle);
+		}
+	}
+}
+
+if ((rot_angle!=0)||(scale_factor!=1)) {
+	FILE* pFile;
+	string OutFileName=argv[1];
+	OutFileName.resize(OutFileName.length()-4);
+	OutFileName=OutFileName+"_mod.p3d";
+	pFile = fopen (OutFileName.c_str(),"w");
+	fprintf(pFile, "%d\t%d\n",ni,nk);
+	for (int v = 0; v < 2; v++) {
+		for (int k = 0; k < nk; k++) {
+			for (int i = 0; i < ni; i++) {
+				fprintf(pFile, "%15.8E\n",values[i][k][v]);
+			}
+		}
+	}
+	fclose (pFile);
+}
+
 FILE* pFile;
 string OutFileName=argv[1];
 OutFileName.resize(OutFileName.length()-3);
@@ -99,7 +147,7 @@ for (int k = 0; k < nk; k++) {
 		if ((k==0) && (values[i][k][0]>=1) && (i>ni/2)) {
 			//do nothing
 		} else {
-			fprintf(pFile, "%15.8E\t%15.8E\n",values[i][k][0]*scale_factor,values[i][k][1]*scale_factor);
+			fprintf(pFile, "%15.8E\t%15.8E\n",values[i][k][0],values[i][k][1]);
 		}
 	}
 }
